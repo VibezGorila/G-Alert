1647d43835a17a5ba37fd802792670ddc8b3bc7f
Vibez_Gorila <58624919+VibezGorila@users.noreply.github.com>
2023-02-23 20:05:39 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(60000) -- This check every 60 second you can change it to your time in miliseconds
+
+        -- These notifications are repeating every 60 seconds if you are below 30% you can change it if you want.
+        TriggerEvent('esx_status:getStatus', 'hunger', function(status)
+            if status.val < 300000 then
+                -- exports['okokNotify']:Alert("Jedlo", "Chod sa najest", 3000, 'warning') -- Delete (--) if you want to use okokNotify
+                  exports["esx_notify"]:Notify("info", 3000, "Go Eat") - You can change it to your message
+            end
+        end)
+
+        TriggerEvent('esx_status:getStatus', 'thirst', function(status)
+            if status.val < 300000 then
+                -- exports['okokNotify']:Alert("Voda", "Chod sa napit", 3000, 'warning') -- Delete (--) if you want to use okokNotify
+                  exports["esx_notify"]:Notify("info", 3000, "Go Drink") - You can change it to your message
+            end
+        end)
+    end
+end)
